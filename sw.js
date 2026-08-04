bd96cba8181793e7de8ae606fa76b74dc335df02
Clark Hermes <victor@youraiguytampa.com>
2026-08-03 20:54:56 -0400

    Open live agent page from CTA

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
    Bump CACHE when you swap the avatar or any voice line, or the booth will keep
    serving the old media from cache. */
-var CACHE = "vicron-booth-v20";
+var CACHE = "vicron-booth-v21";
 
 var ASSETS = [
   "./index.html",
